befc20b4ec97614365cee711dea6adf8993c5814
brezker <brezker10@gmail.com>
2024-09-12 17:41:38 -0700

    Add project files.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiPerson.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPerson.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Person> People { get; set; }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPerson.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
